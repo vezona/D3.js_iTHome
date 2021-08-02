cf2e647ed39bd6b4faff56297b334429f3a138d9
vezonaChin <vezona5tv@gmail.com>
2021-08-02 15:38:16 +0800

    init

---
+++ gulpfile.js/envOptions.js
@@ -0,0 +1,63 @@
+const srcPath = './app';
+const distPath = './dist';
+const nodePath = './node_modules';
+
+let envOptions = {
+  string: 'env',
+  default: {
+    env: 'dev',
+  },
+  copyFile: {
+    src: [
+      `${srcPath}/**/*`,
+      `!${srcPath}/assets/js/**/*.js`,
+      `!${srcPath}/assets/style/**/*.scss`,
+      `!${srcPath}/assets/style/**/*.sass`,
+      `!${srcPath}/**/*.ejs`,
+      `!${srcPath}/**/*.html`,
+    ],
+    path: distPath,
+  },
+  html: {
+    src: [
+      `${srcPath}/**/*.html`,
+    ],
+    ejsSrc: [
+      `${srcPath}/**/*.ejs`,
+    ],
+    path: distPath,
+  },
+  style: {
+    src: [
+      `${srcPath}/assets/style/**/*.scss`,
+      `${srcPath}/assets/style/**/*.sass`,
+    ],
+    path: `${distPath}/assets/style`,
+  },
+  javascript: {
+    src: [
+      `${srcPath}/assets/js/**/*.js`
+    ],
+    concat: 'all.js',
+    path: `${distPath}/assets/js`,
+  },
+  vendors: {
+    src: [
+      `${nodePath}/jquery/dist/**/jquery.min.js`,
+    ],
+    concat: 'vendors.js',
+    path: `${distPath}/assets/js`,
+  },
+  img: {
+    src: [
+      `${srcPath}/assets/images/**/*`,
+    ],
+  },
+  clean: {
+    src: distPath,
+  },
+  browserDir: distPath,
+  deploySrc: `${distPath}/**/*`,
+};
+
+exports.envOptions = envOptions;
