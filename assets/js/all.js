e3e4d6780ae17c1417ffdbf071d6ab7395a86e44
vezonaChin <vezona5tv@gmail.com>
2021-08-24 13:00:07 +0800

    Update 2021-08-24T05:00:01.078Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+$(document).ready(function () {// console.log('HesSchool Hello!');
 });
 //# sourceMappingURL=all.js.map
